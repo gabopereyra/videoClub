17fdea5318a517ecae3ce4fe45fc2cd26eed5fd8
gabopereyra <gabrielp1689@gmail.com>
2022-07-15 10:27:28 -0300

    add: Swagger doc
    SpringFox dependecies
    Swagger Configuration file
    Swagger Controller to use a friendly endpoint to show the information
    application.properties modified to accept Swagger (without annotation
    could not run the project)

---
+++ src/main/java/com/gabo/videoClub/controllers/SwaggerController.java
@@ -0,0 +1,21 @@
+package com.gabo.videoClub.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/api/docs")
+@ApiIgnore
+public class SwaggerController {
+
+    @GetMapping
+    public ModelAndView swaggerPage(HttpServletResponse response) {
+        ModelAndView model = new ModelAndView("redirect:/swagger-ui.html");
+        return model;
+    }
+}
